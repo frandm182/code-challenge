59fa1dd318bd1bb83d3149c6edf1241004885144
Alejo-PC\Alejo <franjdm@gmail.com>
2017-07-26 06:55:54 +0200

    Header, Footer included

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import request from './request';
 import { ARTICLES_QUERY } from './queries';
+import Header from './components/header';
+import Footer from './components/footer';
 
 class App extends Component {
-  // definition
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
     };
   }
-
-  // lifecycle
   componentWillMount() {
     request(ARTICLES_QUERY).then(response => {
       this.setState({ articles: response.data.articles });
     });
   }
-
-  // Renders
   render() {
+    const articles = this.state.articles.map(article => (<div className="album" key={article.id}><div>{article.title}</div><div>{article.author}</div><div >{article.excerpt}</div></div>));
     return (
       <div className="App">
-        <h2>Billin code challenge</h2>
-        <pre>{JSON.stringify(this.state.articles, null, 2)}</pre>
+        <Header />
+        <div className="content">
+          <h2>Billin code challenge</h2>
+          <div>{articles}</div>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/header/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <div className="header"><div className="header-cont">Header</div></div>;
+
+export default Header;
